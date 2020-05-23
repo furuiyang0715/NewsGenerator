e10e546324e27a868d80684057f490a8ea25eb38
furuiyang <furuiyang0715@gmail.com>
2020-05-23 17:52:14 +0800

    improve

--- Finance/main.py
+++ Finance/main.py
@@ -15,13 +15,20 @@ def task():
 def history_task():
     _today = datetime.datetime.combine(datetime.datetime.today(), datetime.time.min)
     _yester_day = _today - datetime.timedelta(days=1)
-    _start = _yester_day - datetime.timedelta(days=180)
+    # _start = _yester_day - datetime.timedelta(days=180)
+    _start = datetime.datetime(2020, 2, 18)
 
+    error_list = []
     _dt = _start
     while _dt < _yester_day:
-        s = Scanner()
-        s.scan(_dt, _dt+datetime.timedelta(days=1))
+        try:
+            s = Scanner()
+            s.scan(_dt, _dt+datetime.timedelta(days=1))
+        except:
+            error_list.append(_dt)
         _dt = _dt+datetime.timedelta(days=1)
+
+    print(error_list)
 
 
 if __name__ == "__main__":
