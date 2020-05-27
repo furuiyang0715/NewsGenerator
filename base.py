8c00fde85c9f0cf318bf4b73918a41bd42820a77
furuiyang <furuiyang0715@gmail.com>
2020-05-27 16:03:29 +0800

    bug fixed

---
+++ base.py
@@ -0,0 +1,225 @@
+import base64
+import hashlib
+import hmac
+import json
+import logging
+import sys
+import time
+import traceback
+
+import requests
+import urllib.parse
+
+sys.path.append("./../")
+from Finance.configs import (SPIDER_MYSQL_HOST, SPIDER_MYSQL_PORT, SPIDER_MYSQL_USER, SPIDER_MYSQL_PASSWORD,
+                             SPIDER_MYSQL_DB, PRODUCT_MYSQL_HOST, PRODUCT_MYSQL_PORT, PRODUCT_MYSQL_USER,
+                             PRODUCT_MYSQL_PASSWORD, PRODUCT_MYSQL_DB, JUY_HOST, JUY_PORT, JUY_USER, JUY_PASSWD,
+                             JUY_DB, DC_HOST, DC_PORT, DC_USER, DC_PASSWD, DC_DB, SECRET, TOKEN, LOCAL)
+from Finance.sql_pool import PyMysqlPoolBase
+
+if LOCAL:
+    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+else:
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+
+class NewsBase(object):
+
+    spider_cfg = {  # 爬虫库
+        "host": SPIDER_MYSQL_HOST,
+        "port": SPIDER_MYSQL_PORT,
+        "user": SPIDER_MYSQL_USER,
+        "password": SPIDER_MYSQL_PASSWORD,
+        "db": SPIDER_MYSQL_DB,
+    }
+
+    product_cfg = {  # 正式库
+        "host": PRODUCT_MYSQL_HOST,
+        "port": PRODUCT_MYSQL_PORT,
+        "user": PRODUCT_MYSQL_USER,
+        "password": PRODUCT_MYSQL_PASSWORD,
+        "db": PRODUCT_MYSQL_DB,
+    }
+
+    # 聚源数据库
+    juyuan_cfg = {
+        "host": JUY_HOST,
+        "port": JUY_PORT,
+        "user": JUY_USER,
+        "password": JUY_PASSWD,
+        "db": JUY_DB,
+    }
+
+    # 数据中心库
+    dc_cfg = {
+        "host": DC_HOST,
+        "port": DC_PORT,
+        "user": DC_USER,
+        "password": DC_PASSWD,
+        "db": DC_DB,
+    }
+
+    def __init__(self):
+        pass
+
+    def _init_pool(self, cfg: dict):
+        """
+        eg.
+        conf = {
+                "host": LOCAL_MYSQL_HOST,
+                "port": LOCAL_MYSQL_PORT,
+                "user": LOCAL_MYSQL_USER,
+                "password": LOCAL_MYSQL_PASSWORD,
+                "db": LOCAL_MYSQL_DB,
+        }
+        :param cfg:
+        :return:
+        """
+        pool = PyMysqlPoolBase(**cfg)
+        return pool
+
+    def contract_sql(self, to_insert: dict, table: str, update_fields: list):
+        ks = []
+        vs = []
+        for k in to_insert:
+            ks.append(k)
+            vs.append(to_insert.get(k))
+        fields_str = "(" + ",".join(ks) + ")"
+        values_str = "(" + "%s," * (len(vs) - 1) + "%s" + ")"
+        base_sql = '''INSERT INTO `{}` '''.format(table) + fields_str + ''' values ''' + values_str
+
+        # 是否在主键冲突时进行更新插入
+        if update_fields:
+            on_update_sql = ''' ON DUPLICATE KEY UPDATE '''
+            update_vs = []
+            for update_field in update_fields:
+                on_update_sql += '{}=%s,'.format(update_field)
+                update_vs.append(to_insert.get(update_field))
+            on_update_sql = on_update_sql.rstrip(",")
+            sql = base_sql + on_update_sql + """;"""
+            vs.extend(update_vs)
+        else:
+            sql = base_sql + """;"""
+
+        return sql, tuple(vs)
+
+    def _save(self, sql_pool, to_insert, table, update_fields):
+        try:
+            insert_sql, values = self.contract_sql(to_insert, table, update_fields)
+            count = sql_pool.insert(insert_sql, values)
+        except:
+            traceback.print_exc()
+            logger.warning("失败")
+        else:
+            if count == 1:
+                logger.info("插入新数据 {}".format(to_insert))
+
+            elif count == 2:
+                logger.info("刷新数据 {}".format(to_insert))
+
+            else:
+                logger.info("已有数据 {} ".format(to_insert))
+                pass
+
+            sql_pool.end()
+            return count
+
+    def get_inner_code_bysecu(self, secu_code):
+        """通过证券代码获取聚源内部编码"""
+        ret = self.inner_code_map.get(secu_code)
+        if not ret:
+            logger.warning("此证券代码 {} 不存在内部编码".format(secu_code))
+            raise
+        return ret
+
+    def get_inner_code_bycompany(self, company_code):
+        """通过公司代码获取聚源内部编码"""
+        ret = self.inner_company_code_map.get(company_code)
+        if not ret:
+            logger.warning("此公司编码 {} 不存在内部编码 ".format(company_code))
+            raise
+        return ret
+
+    @property
+    def inner_code_map(self):
+        """
+        获取 证券代码: 聚源内部编码 映射表
+        https://dd.gildata.com/#/tableShow/27/column///
+        https://dd.gildata.com/#/tableShow/718/column///
+        """
+        juyuan = self._init_pool(self.juyuan_cfg)
+        # 8 是开放式基金
+        sql = 'SELECT SecuCode,InnerCode from SecuMain WHERE SecuCategory in (1, 2, 8) and SecuMarket in (83, 90) and ListedSector in (1, 2, 6, 7);'
+        ret = juyuan.select_all(sql)
+        juyuan.dispose()
+        info = {}
+        for r in ret:
+            key = r.get("SecuCode")
+            value = r.get('InnerCode')
+            info[key] = value
+        return info
+
+    @property
+    def inner_company_code_map(self):
+        """
+        获取 公司编码: 聚源内部编码 映射表
+        """
+        juyuan = self._init_pool(self.juyuan_cfg)
+        sql = '''select CompanyCode, InnerCode from SecuMain WHERE SecuCategory in (1, 2, 8) and SecuMarket in (83, 90) and ListedSector in (1, 2, 6, 7);'''
+        ret = juyuan.select_all(sql)
+        juyuan.dispose()
+        info = {}
+        for r in ret:
+            key = r.get("CompanyCode")
+            value = r.get('InnerCode')
+            info[key] = value
+        return info
+
+    def ding(self, msg):
+        def get_url():
+            timestamp = str(round(time.time() * 1000))
+            secret_enc = SECRET.encode('utf-8')
+            string_to_sign = '{}\n{}'.format(timestamp, SECRET)
+            string_to_sign_enc = string_to_sign.encode('utf-8')
+            hmac_code = hmac.new(secret_enc, string_to_sign_enc, digestmod=hashlib.sha256).digest()
+            sign = urllib.parse.quote_plus(base64.b64encode(hmac_code))
+            url = 'https://oapi.dingtalk.com/robot/send?access_token={}&timestamp={}&sign={}'.format(
+                TOKEN, timestamp, sign)
+            return url
+
+        url = get_url()
+        header = {
+            "Content-Type": "application/json",
+            "Charset": "UTF-8"
+        }
+        message = {
+            "msgtype": "text",
+            "text": {
+                "content": "{}@15626046299".format(msg)
+            },
+            "at": {
+                "atMobiles": [
+                    "15626046299",
+                ],
+                "isAtAll": False
+            }
+        }
+        message_json = json.dumps(message)
+        resp = requests.post(url=url, data=message_json, headers=header)
+        if resp.status_code == 200:
+            logger.info("钉钉发送消息成功: {}".format(msg))
+        else:
+            logger.warning("钉钉消息发送失败")
+
+    def total_company_codes(self):
+        """获取涵盖在统计范围内的全部公司代码 按照文档的写法 是只需要 A 股的"""
+        juyuan = self._init_pool(self.juyuan_cfg)
+        sql = '''select CompanyCode, SecuCode, InnerCode from secumain where SecuCategory = 1; '''
+        ret = juyuan.select_all(sql)
+        _map = {}
+        for r in ret:
+            company_code = r.get("CompanyCode")
+            _map[company_code] = (r.get("SecuCode"), r.get("InnerCode"))
+
+        return _map
--- north_fund/scan.py
+++ north_fund/scan.py
@@ -1,6 +1,6 @@
 import datetime
 
-from Finance.base import NewsBase, logger
+from Finance.base import NewsBase
 
 
 class NorthFund(NewsBase):
@@ -8,99 +8,30 @@ class NorthFund(NewsBase):
         super(NorthFund, self).__init__()
         self.source_table = 'hkland_flow'
         self.target_table = 'news_generate_flownorth'
-        self.fields = ['Title', "Content", "DateTime", 'Threshold']
-
-    def fetch_data(self, start_dt, end_dt):
-        sql = '''select DateTime,Netinflow,ShHkFlow,SzHkFlow from {} \
-        where Category=2 and DateTime>='{}' and DateTime<='{}'\
-        and Netinflow=(select max(Netinflow) from {} where Category=2 and DateTime>='{}' and DateTime<='{}');
-        '''.format(self.source_table, start_dt, end_dt, self.source_table, start_dt, end_dt)
-        client = self._init_pool(self.dc_cfg)
-        ret = client.select_one(sql)
-        client.dispose()
-        return ret
+        self.fields = ['Date', 'Threshold', 'Title', "Content", "DateTime"]
+        # 阈值 50亿  80亿  120亿
+        self.thre1 = 50*10**4
+        self.thre2 = 80*10**4
+        self.thre3 = 120*10**4
 
     def _create_table(self):
         client = self._init_pool(self.product_cfg)
         sql = '''
         CREATE TABLE IF NOT EXISTS `{}` (
           `id` int(11) NOT NULL AUTO_INCREMENT,
+          `Date` datetime NOT NULL COMMENT '日期', 
+          `Threshold` decimal(19,0) NOT NULL COMMENT '阈值(单位:万)', 
+          `DateTime` datetime NOT NULL COMMENT '达到阈值的分钟时间点', 
           `Title` varchar(64) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '生成文章标题', 
-          `Content` text CHARACTER SET utf8 COLLATE utf8_bin COMMENT '生成文章正文',
-          `DateTime` datetime NOT NULL COMMENT '达到阈值的分钟时间点',
-          `Positive` int NOT NULL COMMENT '北向资金流入流出标志: 1流入0流出 ',          
-          `Threshold` int NOT NULL COMMENT '阈值(单位:亿)',          
+          `Content` text CHARACTER SET utf8 COLLATE utf8_bin COMMENT '生成文章正文',           
           `CREATETIMEJZ` datetime DEFAULT CURRENT_TIMESTAMP,
           `UPDATETIMEJZ` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
            PRIMARY KEY (`id`),
-           UNIQUE KEY `date_time` (`DateTime`)
+           UNIQUE KEY `dt_thre` (`Date`, `Threshold`)
         ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='北向资金流入资讯生成';
         '''.format(self.target_table)
         client.insert(sql)
         client.dispose()
-
-    def start(self):
-        # 建表
-        self._create_table()
-
-        # (1) 获取当前的时间点
-        _now = datetime.datetime.now()
-        # (2) 从当天的开始时间(或者北向资金的开始时间 9 点半)到当前时间之间的全部数据中最大的一只
-        _start = datetime.datetime(_now.year, _now.month, _now.day, 9, 30, 0)
-        data = self.fetch_data(_start, _now)
-
-        netinflow = data.get("Netinflow")
-        shhkflow = data.get("ShHkFlow")
-        szhkflow = data.get("SzHkFlow")
-        date_time = data.get("DateTime")
-
-        flag_str_netin = "流入" if netinflow > 0 else "流出"
-        flag_str_shhk = "流入" if shhkflow > 0 else "流出"
-        flag_str_szhk = "流入" if szhkflow > 0 else "流出"
-
-        positive = 1 if netinflow > 0 else 0
-
-        netinflow = abs(netinflow)
-        shhkflow = abs(shhkflow)
-        szhkflow = abs(szhkflow)
-
-        if netinflow >= 120 * 10 ** 4:     # 120 亿
-            _threshold = 120
-        elif netinflow >= 80 * 10 ** 4:    # 80 亿
-            _threshold = 80
-        elif netinflow >= 50 * 10 ** 4:     # 50 亿
-            _threshold = 50
-        else:
-            logger.info("{} 未达到最近触发阈值".format(netinflow))
-            return
-
-        netinflow_info = self.re_data2str(netinflow)
-        shhkflow_info = self.re_data2str(shhkflow)
-        szhkflow_info = self.re_data2str(szhkflow)
-
-        title = "截止目前北向资金当前净" + flag_str_netin + "{}元".format(netinflow_info)
-        content = "截至{}月{}日{}时{}分，北向资金净{}{}元，其中沪股通{}{}元，深股通{}{}元。".format(
-            date_time.month, date_time.day, date_time.hour, date_time.minute,
-            flag_str_netin, netinflow_info,
-            flag_str_shhk, shhkflow_info, flag_str_szhk, szhkflow_info
-        )
-
-        # 获取数据库中已有的最大阈值 根据当前是流入和流出进行对应判断
-        client = self._init_pool(self.product_cfg)
-        sql = '''select max(Threshold) as max_threshold from {} where DateTime >= '{}' and Positive = {}; '''.format(
-            self.target_table, _start, positive)
-        ret = client.select_one(sql)
-        max_threshold = ret.get("max_threshold")
-        if not max_threshold or max_threshold < _threshold:
-            item = dict()
-            item['Title'] = title
-            item['Content'] = content
-            item['DateTime'] = date_time
-            item['Positive'] = positive
-            item['Threshold'] = _threshold  # 单位: 亿
-            self._save(client, item, self.target_table, self.fields)
-        else:
-            logger.info("非首次突破阈值不生成")
 
     def re_data2str(self, data):
         data = abs(data)
@@ -109,17 +40,135 @@ class NorthFund(NewsBase):
         data = float("%.2f" % data)
         return "{}{}".format(data, unit)
 
+    def fetch_data(self, start_dt, end_dt):
+        client = self._init_pool(self.dc_cfg)
+        positive = {}
+        negative = {}
+        for data in (self.thre1, self.thre2, self.thre3):
+            sql = '''select DateTime,Netinflow,ShHkFlow,SzHkFlow from {} \
+            where Category=2 and DateTime>='{}' and DateTime<='{}'\
+            and Netinflow >= {} order by Datetime asc limit 1;
+            '''.format(self.source_table, start_dt, end_dt, data)
+            info = client.select_one(sql)
+            positive[data] = info
+
+        for data in ((-1)*self.thre1, (-1)*self.thre2, (-1)*self.thre3):
+            sql = '''select DateTime,Netinflow,ShHkFlow,SzHkFlow from {} \
+            where Category=2 and DateTime>='{}' and DateTime<='{}'\
+            and Netinflow <= {} order by Datetime asc limit 1;
+            '''.format(self.source_table, start_dt, end_dt, data)
+            info = client.select_one(sql)
+            negative[data] = info
+
+        client.dispose()
+        return positive, negative
+
+    def produce(self, key, item):
+        netinflow = item.get("Netinflow")
+        shhkflow = item.get("ShHkFlow")
+        szhkflow = item.get("SzHkFlow")
+        date_time = item.get("DateTime")
+        flag_str_netin = "流入" if netinflow > 0 else "流出"
+        flag_str_shhk = "流入" if shhkflow > 0 else "流出"
+        flag_str_szhk = "流入" if szhkflow > 0 else "流出"
+        netinflow_info = self.re_data2str(abs(netinflow))
+        shhkflow_info = self.re_data2str(abs(shhkflow))
+        szhkflow_info = self.re_data2str(abs(szhkflow))
+        title = "截止目前北向资金当前净" + flag_str_netin + "{}元".format(netinflow_info)
+        content = "截至{}月{}日{}时{}分，北向资金净{}{}元，其中沪股通{}{}元，深股通{}{}元。".format(
+            date_time.month, date_time.day, date_time.hour, date_time.minute,
+            flag_str_netin, netinflow_info,
+            flag_str_shhk, shhkflow_info, flag_str_szhk, szhkflow_info
+        )
+
+        item = dict()
+        item['Date'] = datetime.datetime.combine(date_time, datetime.time.min)
+        item['Threshold'] = key
+        item['Title'] = title
+        item['Content'] = content
+        item['DateTime'] = date_time
+        return item
+
+    def get_final_data(self, end_dt):
+        client = self._init_pool(self.dc_cfg)
+        sql = '''select DateTime,Netinflow,ShHkFlow,SzHkFlow from {} \
+        where Category=2 and DateTime='{}';
+        '''.format(self.source_table, end_dt)
+        final_data = client.select_one(sql)
+        client.dispose()
+        if not final_data:
+            return None
+
+        netinflow = final_data.get("Netinflow")
+        shhkflow = final_data.get("ShHkFlow")
+        szhkflow = final_data.get("SzHkFlow")
+        date_time = final_data.get("DateTime")
+        flag_str_netin = "流入" if netinflow > 0 else "流出"
+        flag_str_shhk = "流入" if shhkflow > 0 else "流出"
+        flag_str_szhk = "流入" if szhkflow > 0 else "流出"
+        netinflow_info = self.re_data2str(abs(netinflow))
+        shhkflow_info = self.re_data2str(abs(shhkflow))
+        szhkflow_info = self.re_data2str(abs(szhkflow))
+        title = "截至今日收盘,北向资金净" + flag_str_netin + "{}元".format(netinflow_info)
+        content = "截至{}月{}日，北向资金净{}{}元，其中沪股通{}{}元，深股通{}{}元。".format(
+            date_time.month, date_time.day,
+            flag_str_netin, netinflow_info,
+            flag_str_shhk, shhkflow_info,
+            flag_str_szhk, szhkflow_info
+        )
+        item = dict()
+        item['Date'] = datetime.datetime.combine(end_dt, datetime.time.min)
+        item['Threshold'] = 0
+        item['Title'] = title
+        item['Content'] = content
+        item['DateTime'] = date_time
+        return item
+
+    def start(self):
+        self._create_table()
+        _now = datetime.datetime.now()
+        _start = datetime.datetime(_now.year, _now.month, _now.day, 9, 30, 0)
+        _end = datetime.datetime(_now.year, _now.month, _now.day, 15, 0, 0)
+        client = self._init_pool(self.product_cfg)
+        if _now > _end:
+            # 入库北向资金当日收盘流入
+            item = self.get_final_data(_end)
+            if item:
+                self._save(client, item, self.target_table, self.fields)
+
+        positive, negative = self.fetch_data(_start, _now)
+        positive.update(negative)
+        for key, value in positive.items():
+            if value:
+                item = self.produce(key, value)
+                self._save(client, item, self.target_table, self.fields)
+        client.dispose()
+
+    def history(self):
+        client = self._init_pool(self.product_cfg)
+        s_day = datetime.datetime(2020, 1, 1)
+        e_day = datetime.datetime(2020, 5, 26)
+        _day = s_day
+        while _day <= e_day:
+
+            positive, negative = self.fetch_data(datetime.datetime(_day.year, _day.month, _day.day, 9, 30),
+                                                 datetime.datetime(_day.year, _day.month, _day.day, 15, 0))
+            positive.update(negative)
+            for key, value in positive.items():
+                if value:
+                    item = self.produce(key, value)
+                    self._save(client, item, self.target_table, self.fields)
+
+            item = self.get_final_data(datetime.datetime(_day.year, _day.month, _day.day, 15, 0))
+            if item:
+                self._save(client, item, self.target_table, self.fields)
+
+            _day += datetime.timedelta(days=1)
+            print()
+            print()
+
 
 if __name__ == "__main__":
     north = NorthFund()
     north.start()
-
-    # # 测试数据转换
-    # d1 = 500
-    # print(north.re_data2str(d1))
-    #
-    # d2 = 5678.8899
-    # print(north.re_data2str(d2))
-    #
-    # d3 = 4500008.3736
-    # print(north.re_data2str(d3))
+    north.history()
