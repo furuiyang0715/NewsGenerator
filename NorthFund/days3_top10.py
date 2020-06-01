40316656090c2dacfa149ef710de50a6a4402f4c
furuiyang <furuiyang0715@gmail.com>
2020-06-01 17:19:55 +0800

    增加接口数据

--- NorthFund/days3_top10.py
+++ NorthFund/days3_top10.py
@@ -32,6 +32,7 @@ class Stocks3DaysTop10(NewsBase):
         self.idx_table = 'stk_quot_idx'
         self.dc_client = None
         self.target_client = None
+        self.juyuan_client = None
         self.target_table = 'news_generate_stocks3daystop10'
 
     def _dc_init(self):
@@ -40,11 +41,16 @@ class Stocks3DaysTop10(NewsBase):
     def _target_init(self):
         self.target_client = self._init_pool(self.product_cfg)
 
+    def _juyuan_init(self):
+        self.juyuan_client = self._init_pool(self.juyuan_cfg)
+
     def __del__(self):
         if self.dc_client:
             self.dc_client.dispose()
         if self.target_client:
             self.target_client.dispose()
+        if self.juyuan_client:
+            self.juyuan_client.dispose()
 
     def _create_table(self):
         client = self._init_pool(self.product_cfg)
@@ -62,14 +68,21 @@ class Stocks3DaysTop10(NewsBase):
         client.insert(sql)
         client.dispose()
 
-    def get_changepercactual(self, secu_code):
-        inner_code = self.get_inner_code_bysecu(secu_code)
+    def get_juyuan_codeinfo(self, secu_code):
+        self._juyuan_init()
+        sql = 'SELECT SecuCode,InnerCode, SecuAbbr from SecuMain WHERE SecuCategory in (1, 2, 8) \
+and SecuMarket in (83, 90) \
+and ListedSector in (1, 2, 6, 7) and SecuCode = "{}";'.format(secu_code)
+        ret = self.juyuan_client.select_one(sql)
+        return ret.get('InnerCode'), ret.get("SecuAbbr")
+
+    def get_changepercactual(self, inner_code):
         self._dc_init()
-        sql = '''select Close, ChangePercActual from {} where InnerCode = {} and Date <= '{}' order by Date desc limit 1; 
+        sql = '''select Close, ChangePercActual from {} where InnerCode = '{}' and Date <= '{}' order by Date desc limit 1; 
         '''.format(self.idx_table, inner_code, self.day)    # 因为假如今天被机构首次评级立即发布,未收盘前拿到的是昨天的行情数据, 收盘手拿到的是今天的
         ret = self.dc_client.select_one(sql)
         changepercactual = self.re_decimal_data(ret.get("ChangePercActual"))
-        return inner_code, changepercactual
+        return changepercactual
 
     def start(self):
         self._create_table()
@@ -86,10 +99,12 @@ class Stocks3DaysTop10(NewsBase):
                 if i.type == 1:
                     logger.info("value: {}".format(struct.unpack("<f", i.value)[0]))
                     secu_code = one.stock_code[2:]
-                    inner_code, _changepercactual = self.get_changepercactual(secu_code)
+                    inner_code, secu_abbr = self.get_juyuan_codeinfo(secu_code)
+                    _changepercactual = self.get_changepercactual(inner_code)
                     item['value'] = struct.unpack("<f", i.value)[0]
                     item['secu_code'] = secu_code
                     item['inner_code'] = inner_code
+                    item['secu_abbr'] = secu_abbr
                     item['changepercactual'] = _changepercactual
                     # print(type(_changepercactual))
                     rank_map[rank_num] = item
@@ -97,11 +112,11 @@ class Stocks3DaysTop10(NewsBase):
                 elif i.type == 3:
                     logger.info(bytes.fromhex(i.value.hex()).decode("utf-8"))
 
-        # print(pprint.pformat(rank_map))
         rank_info = json.dumps(rank_map, ensure_ascii=False)
         data = {"Date": self.day, "RankInfo": rank_info}
-        # for k, v in data.items():
-        #     print(k, ">>", v)
+
+        for k, v in rank_map.items():
+            print(k, ">>", v)
 
         self._target_init()
         self._save(self.target_client, data, self.target_table, ['Date', "RankInfo"])
