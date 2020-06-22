8a2cb5f6f4a9c481fb85b02b65c307716ec87e82
furuiyang <furuiyang0715@gmail.com>
2020-06-22 14:43:48 +0800

    生成内容

--- Funds/morning_top10.py
+++ Funds/morning_top10.py
@@ -24,16 +24,6 @@ class MorningTop10(NewsBase):
             # heartbeat=3,
         )
 
-    @staticmethod
-    def p_code(code):
-        if code.startswith("6"):
-            p_code = "SH" + code
-        elif code.startswith("0") or code.startswith("3"):
-            p_code = "SZ" + code
-        else:
-            return None
-        return p_code
-
     def get_rank10(self):
         rank = Rank.sync_get_rank_net_purchase_by_code(
             self.client, offset=0, count=10, stock_code_array=["$$沪深A股"]
@@ -69,68 +59,49 @@ class MorningTop10(NewsBase):
 
         return items
 
-    # def update_real_risepercent(self, top10info: dict):
-    #     """
-    #     查询并且更入股票的实时涨跌幅
-    #     """
-    #     p_codes = [i[0] for i in top10info.values()]
-    #     datas = []
-    #     rank = Rank.sync_get_rank_by_rise_scope(self.client, stock_code_array=p_codes)
-    #     for one in rank.row:
-    #         data = dict()
-    #         secu_code = one.stock_code[2:]
-    #         data['secu_code'] = secu_code
-    #         data['secu_abbr'] = self.get_juyuan_codeinfo(secu_code)[1]
-    #         rise_p = None
-    #         for i in one.data:
-    #             if i.type == 1:
-    #                 rise_p = struct.unpack("<f", i.value)[0]
-    #             elif i.type == 4:
-    #                 rise_p = struct.unpack("<i", i.value)[0]
-    #             elif i.type == 2:
-    #                 rise_p = struct.unpack("<d", i.value)[0]
-    #             elif i.type == 3:
-    #                 rise_p = bytes.fromhex(i.value.hex()).decode("utf-8")
-    #             else:
-    #                 raise ValueError("解析错误")
-    #         data['rise_percent'] = rise_p
-    #         topinfo = top10info.get(secu_code)
-    #         data['rank'] = topinfo[1]
-    #         data["main_buy_value"] = topinfo[2]
-    #         datas.append(data)
-    #     return datas
+    def get_content(self, datas: list):
+        """
+        早盘主力十大净买个股
+        标题：4月15日早盘，十大主力净买个股（截取今日10点半前数据）
+        条件：取每天十点半时，主力净买额前十的个股
 
-    # def get_content(self, datas):
-    #     """
-    #     早盘主力十大净买个股
-    #     标题：4月15日早盘，十大主力净买个股（截取今日10点半前数据）
-    #     条件：取每天十点半时，主力净买额前十的个股
-    #
-    #     内容：
-    #     4月15日大单金额流入前十名个股如下，数据取自（4月15日 10:30）
-    #     山河药辅（300452）+1.54%，主力净买额1200万    (实时涨跌幅)
-    #     皇氏集团（002329）+1.54%，主力净买额1200万
-    #     以岭药业（002603）+1.54%，主力净买额1200万
-    #     海伦哲（300201）  +1.54%，主力净买额1200万
-    #     宝色股份（300402）+1.54%，主力净买额1200万
-    #     延江股份（300658）+1.54%，主力净买额1200万
-    #     龙宇燃油（603003）+1.54%，主力净买额1200万
-    #     泉峰汽车（603982）+1.54%，主力净买额1200万
-    #     """
-    #     _today_now = datetime.datetime.now()
-    #     _month = _today_now.month
-    #     _day = _today_now.day
-    #     title = "{}月{}日早盘，十大主力净买个股（截取今日10点半前数据）".format(_month, _day)
-    #     base_content = '{}月{}日大单金额流入前十名个股如下，数据取自（{}月{}日 10:30）\n'.format(_month, _day, _month, _day)
-    #     for data in datas:
-    #         content = '山河药辅（300452）+1.54%，主力净买额1200万'
+        内容：
+        4月15日大单金额流入前十名个股如下，数据取自（4月15日 10:30）
+        山河药辅（300452）+1.54%，主力净买额1200万    (实时涨跌幅)
+        皇氏集团（002329）+1.54%，主力净买额1200万
+        以岭药业（002603）+1.54%，主力净买额1200万
+        海伦哲（300201）  +1.54%，主力净买额1200万
+        宝色股份（300402）+1.54%，主力净买额1200万
+        延江股份（300658）+1.54%，主力净买额1200万
+        龙宇燃油（603003）+1.54%，主力净买额1200万
+        泉峰汽车（603982）+1.54%，主力净买额1200万
+        """
+        final = dict()
+        _today = datetime.datetime.combine(datetime.datetime.now(), datetime.time.min)
+        final["Date"] = _today
+        _month = _today.month
+        _day = _today.day
+
+        title = "{}月{}日早盘，十大主力净买个股（截取今日10点半前数据）".format(_month, _day)
+        final['Title'] = title
+        base_content = '{}月{}日大单金额流入前十名个股如下，数据取自（{}月{}日 10:30）\n'.format(_month, _day, _month, _day)
+        # {'secu_code': 'SZ002129', 'secu_abbr': '中环股份', 'main_buy': 688491328.0,
+        # 'rise_percent': 10.025842666625977, 'rank_num': 1}
+        # 山河药辅（300452）+1.54%，主力净买额1200万
+        for data in datas:
+            content = '{}（{}）{}%，主力净买额{}\n'.format(data.get("secu_abbr"), data.get("secu_code")[2:], data.get("rise_percent"), data.get("main_buy"))
+            base_content += content
+
+        final["Content"] = base_content
+        return final
 
     def start(self):
         top10info = self.get_rank10()
         for one in top10info:
             print(one)
 
-        # to_insert = self.get_content(datas)
+        to_insert = self.get_content(top10info)
+        print(to_insert)
 
 
 if __name__ == "__main__":
