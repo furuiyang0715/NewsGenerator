52f75434f3cdddda3907cc5fde394738c9a35637
furuiyang <furuiyang0715@gmail.com>
2020-06-24 16:07:03 +0800

    add some

--- Funds/open_unusual.py
+++ Funds/open_unusual.py
@@ -31,12 +31,32 @@ class OpenUnusual(NewsBase):
             max_retry=-1,
             # heartbeat=3,
         )
+        # self.stock_monitor_status_map = {
+        #     1: "直线拉升",
+        #     2: "直线拉升",
+        #     3: "疯涨停板",
+        #     4: "涨停板",
+        #     5: "冲高回落",
+        #     6: "快速跳水",
+        #     7: "快速跳水",
+        # }
+
+        # self.stock_monitor_status_map = {
+        #     1: "竞价大涨",
+        #     2: "竞价大涨",
+        #     3: "竞价涨停",
+        #     4: "竞价涨停",
+        #     5: "竞价大涨",
+        #     6: "竞价大涨",
+        #     7: "竞价大涨",
+        # }
 
     def get_all_block_stats(self):
         base_time = datetime.datetime.now()
         target_time = base_time - datetime.timedelta(days=0,
                                                      # hours=8,
                                                      )
+        target_time = datetime.datetime(2020, 6, 24, 9, 36, 0)
         now_ts = int(time.mktime(target_time.timetuple()))
         res = TopicInvest.sync_get_topic_info(self.client, ts=now_ts)
 
@@ -49,8 +69,6 @@ class OpenUnusual(NewsBase):
                 code = one.stock_code
                 stats = one.status
                 block_stats_map[block_code].append({"code": code, "stats": stats})
-
-        print(pprint.pformat(block_stats_map))
         return block_stats_map
 
     def get_block_rise_map(self, all_block_codes):
@@ -73,11 +91,9 @@ class OpenUnusual(NewsBase):
                     print(bytes.fromhex(i.value.hex()).decode("utf-8"))
                 if value and value > 1.5:
                     block_rise_map[block_code] = value
-
-        print(pprint.pformat(block_rise_map))
         return block_rise_map
 
-    def get_content(self, datas: dict, block_rise_map):
+    def get_content(self, datas, block_rise_map):
         """
         {'IX850039': [{'code': 'SZ300459', 'stats': 3},
                       {'code': 'SH603003', 'stats': 1},
@@ -91,34 +107,42 @@ class OpenUnusual(NewsBase):
         5G板块开盘活跃，盘口涨幅达1.5%，华星创业一字涨停，麦捷科技大涨9%，有方科技涨8%。
         """
         self._theme_init()
+        block_names = []
+        rows_str = ''
         for block_code, block_info in datas.items():
-            sql = """select name from block where code = '{}';""".format(block_code)
-            block_name = self.theme_client.select_one(sql)
-            if block_name:
-                block_name = block_name.get("name")
-            else:
-                raise
-            block_rise = self.re_decimal_data(block_rise_map.get(block_code))
-            title = '{}开盘活跃,涨幅高达{}%'.format(block_name, block_rise)
+            if block_code in block_rise_map:
+                sql = """select name from block where code = '{}';""".format(block_code)
+                block_name = self.theme_client.select_one(sql)
+                if block_name:
+                    block_name = block_name.get("name")
+                    block_names.append(block_name)
+                else:
+                    raise
+                block_rise = self.re_decimal_data(block_rise_map.get(block_code))
+                count = 1
+                base_content = '{}版块竞价涨幅达{}%，'.format(block_name, block_rise)
+                for one in block_info:
+                    if count > 3:
+                        break
+                    code = one.get("code")
+                    stats = one.get("stats")
+                    if count == 1:
+                        content = self.get_code_rise_info(code, stats, lead=1)
+                    else:
+                        content = self.get_code_rise_info(code, stats, lead=0)
+                    base_content += content
+                    count += 1
 
-            count = 1
-            base_content = ''
-            for one in block_info:
-                if count > 3:
-                    break
-                code = one.get("code")
-                stats = one.get("stats")
-                content = self.get_code_rise_info(code)
-                base_content += content
-                count += 1
-            content = title + "," + base_content
+                # ，后面有个空格
+                row = base_content[:-2] + "。\n"
+                rows_str += row
 
-            print(block_name)
-            print(title)
-            print(content)
-            print(stats)
+        title_format = "今日竞价表现：" + ("{}、"*len(block_names))[:-1] + "板块活跃"
+        title = title_format.format(*block_names)
+        print(title)
+        print(rows_str)
 
-    def get_code_rise_info(self, code):
+    def get_code_rise_info(self, code, stats, lead=1):
         """
         查询股票的实时涨跌幅
         """
@@ -138,20 +162,26 @@ class OpenUnusual(NewsBase):
                     value = bytes.fromhex(i.value.hex()).decode("utf-8")
                 else:
                     raise
-                return "{}涨{}%,".format(secu_abbr, self.re_decimal_data(value))
+                if lead:
+                    if stats in (3, 4):
+                        return "{}竞价涨停, ".format(secu_abbr)
+                    else:
+                        return "{}竞价大涨{}%, ".format(secu_abbr, self.re_decimal_data(value))
+                else:
+                    return "{}跟涨{}%, ".format(secu_abbr, self.re_decimal_data(value))
 
     def start(self):
         all_block_stats_map = self.get_all_block_stats()
+        # print(pprint.pformat(all_block_stats_map))
         all_block_codes = list(all_block_stats_map.keys())
-        block_rise_map = self.get_block_rise_map(all_block_codes)
-        rise_block_codes = list(block_rise_map.keys())
+        # print(all_block_codes)
 
-        final_items = {}
-        for key, code_infos in all_block_stats_map.items():
-            if key in rise_block_codes:
-                final_items[key] = code_infos
-        if final_items:
-            self.get_content(final_items, block_rise_map)
+        block_rise_map = self.get_block_rise_map(all_block_codes)
+        # print(pprint.pformat(block_rise_map))
+        rise_block_codes = list(block_rise_map.keys())
+        # print(rise_block_codes)
+
+        self.get_content(all_block_stats_map, block_rise_map)
 
 
 if __name__ == "__main__":
