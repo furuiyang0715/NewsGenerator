0e7e479b6d28084dd301e3c1d2c52f9fe1e143af
furuiyang <furuiyang0715@gmail.com>
2020-06-24 14:43:44 +0800

    Add some

--- Funds/open_unusual.py
+++ Funds/open_unusual.py
@@ -1,25 +1,15 @@
-'''
-开盘异动盘口
-条件：取主题猎手-盘口异动9：36分时，出现涨停个股且涨幅大于1.5%的异动盘口，(获取领涨股？ )再取两个涨幅最高的跟涨个股
-标题：5G板块开盘活跃,涨幅高达1.5%
-内容：
-5G板块开盘活跃，盘口涨幅达1.5%，华星创业一字涨停，麦捷科技大涨9%，有方科技涨8%。
-
-
-流程：
-第一步: 拿到所有异动
-第二步，查所有板块的实时涨幅，筛选出大于 1.5 的
-第三步，把筛选完的数据，再根据个股的异动状态是涨停板或者封涨停板的，筛出来
-第四步再去拿跟风股的实时涨幅
-
-'''
-
 import datetime
+import os
 import pprint
 import struct
 import sys
 import time
 from collections import defaultdict
+
+cur_path = os.path.split(os.path.realpath(__file__))[0]
+file_path = os.path.abspath(os.path.join(cur_path, ".."))
+sys.path.insert(0, file_path)
+
 
 from PyAPI.JZpyapi import const
 from PyAPI.JZpyapi.apis.report import TopicInvest, Rank
@@ -64,7 +54,9 @@ class OpenUnusual(NewsBase):
         return block_stats_map
 
     def get_block_rise_map(self, all_block_codes):
-        # 查所有板块的实时涨幅，筛选出大于 1.5 的
+        """
+        查所有板块的实时涨幅，筛选出大于 1.5 的
+        """
         block_rise_map = {}
         rank = Rank.sync_get_rank_by_bk(self.client,
                                         offset=0,
@@ -85,50 +77,13 @@ class OpenUnusual(NewsBase):
         print(pprint.pformat(block_rise_map))
         return block_rise_map
 
-    def start(self):
-        '''
-        enum stock_monitor_status
-        {
-        stock_monitor_status_null = 0;
-        // 直线拉升
-        stock_monitor_status_zxls_level_1 = 1;
-        stock_monitor_status_zxls_level_2 = 2;
-        // 疯涨停板
-        stock_monitor_status_fztb_level = 3;
-        // 涨停板
-        stock_monitor_status_ztb_level = 4;
-        // 冲高回落
-        stock_monitor_status_cghl_level = 5;
-        // 快速跳水
-        stock_monitor_status_ksts_level_1 = 6;
-        stock_monitor_status_ksts_level_2 = 7;
-        stock_monitor_status_end = 1000;
-        }
-        '''
-        all_block_stats_map = self.get_all_block_stats()
-        all_block_codes = list(all_block_stats_map.keys())
-        block_rise_map = self.get_block_rise_map(all_block_codes)
-        rise_block_codes = list(block_rise_map.keys())
-
-        print("# " * 20)
-        final_items = {}
-        for key, code_infos in all_block_stats_map.items():
-            if key in rise_block_codes:
-                final_items[key] = code_infos
-                # for code_info in code_infos:
-                #     if code_info.get("stats") in (3, 4):
-                #         final_items[key] = code_infos
-                #         break    # 退出内层循环
-
-        # print(pprint.pformat(final_items))
-        if final_items:
-            self.get_content(final_items, block_rise_map)
-
     def get_content(self, datas: dict, block_rise_map):
         """
         {'IX850039': [{'code': 'SZ300459', 'stats': 3},
                       {'code': 'SH603003', 'stats': 1},
                       {'code': 'SH600070', 'stats': 1}],
+                      ......
+
 
         eg.
         标题：5G板块开盘活跃,涨幅高达1.5%
@@ -152,6 +107,7 @@ class OpenUnusual(NewsBase):
                 if count > 3:
                     break
                 code = one.get("code")
+                stats = one.get("stats")
                 content = self.get_code_rise_info(code)
                 base_content += content
                 count += 1
@@ -160,9 +116,12 @@ class OpenUnusual(NewsBase):
             print(block_name)
             print(title)
             print(content)
+            print(stats)
 
     def get_code_rise_info(self, code):
-        # 查询股票的实时涨跌幅
+        """
+        查询股票的实时涨跌幅
+        """
         rank = Rank.sync_get_rank_by_rise_scope(
             self.client, stock_code_array=[code]
         )
@@ -180,6 +139,19 @@ class OpenUnusual(NewsBase):
                 else:
                     raise
                 return "{}涨{}%,".format(secu_abbr, self.re_decimal_data(value))
+
+    def start(self):
+        all_block_stats_map = self.get_all_block_stats()
+        all_block_codes = list(all_block_stats_map.keys())
+        block_rise_map = self.get_block_rise_map(all_block_codes)
+        rise_block_codes = list(block_rise_map.keys())
+
+        final_items = {}
+        for key, code_infos in all_block_stats_map.items():
+            if key in rise_block_codes:
+                final_items[key] = code_infos
+        if final_items:
+            self.get_content(final_items, block_rise_map)
 
 
 if __name__ == "__main__":
