7d05b5c68631a6f4dc38b9367fa7f5f7ed6b3a93
furuiyang <furuiyang0715@gmail.com>
2020-06-28 14:23:04 +0800

    bug fixed

--- Funds/morning_top10.py
+++ Funds/morning_top10.py
@@ -19,6 +19,7 @@ from configs import API_HOST, AUTH_USERNAME, AUTH_PASSWORD
 
 
 class MorningTop10(NewsBase):
+    """早盘主力十大净买个股"""
     def __init__(self):
         super(MorningTop10, self).__init__()
         self.client = SyncSocketClient(
@@ -31,22 +32,50 @@ class MorningTop10(NewsBase):
             max_retry=-1,
             # heartbeat=3,
         )
-        self.target_table = 'news_generate_morningtop10'
-        self.fields = ["Date", "Title", "Content"]
+        self.day = datetime.datetime.combine(datetime.datetime.now(), datetime.time.min)
+        # self.target_table = 'news_generate_morningtop10'
+        # self.fields = ["Date", "Title", "Content"]
+        self.target_table = 'news_generate'
+        self.fields = ['Date', 'Title', 'Content', 'NewsType', 'NewsJson']
+
+    # def _create_table(self):
+    #     self._target_init()
+    #     sql = '''
+    #     CREATE TABLE IF NOT EXISTS `{}` (
+    #       `id` int(11) NOT NULL AUTO_INCREMENT,
+    #       `Date` datetime NOT NULL COMMENT '资讯发布时间',
+    #       `Title` varchar(64) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '生成文章标题',
+    #       `Content` text CHARACTER SET utf8 COLLATE utf8_bin COMMENT '生成文章正文',
+    #       `CREATETIMEJZ` datetime DEFAULT CURRENT_TIMESTAMP,
+    #       `UPDATETIMEJZ` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    #        PRIMARY KEY (`id`),
+    #        UNIQUE KEY `un2` (`Date`)
+    #     ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='资讯-早盘主力十大净买个股';
+    #     '''.format(self.target_table)
+    #     self.target_client.insert(sql)
+    #     self.target_client.end()
 
     def _create_table(self):
+        """
+        新闻类型：
+        1:  三日连续净流入前10个股
+        2:  连板股今日竞价表现
+        3: 早盘主力十大净买个股
+        """
         self._target_init()
         sql = '''
         CREATE TABLE IF NOT EXISTS `{}` (
           `id` int(11) NOT NULL AUTO_INCREMENT,
-          `Date` datetime NOT NULL COMMENT '资讯发布时间', 
+          `NewsType` int NOT NULL COMMENT '新闻类型',
+          `Date` datetime NOT NULL COMMENT '日期', 
+          `NewsJson` json  DEFAULT  NULL COMMENT 'json 格式的新闻数据体', 
           `Title` varchar(64) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '生成文章标题', 
-          `Content` text CHARACTER SET utf8 COLLATE utf8_bin COMMENT '生成文章正文',
+          `Content` text CHARACTER SET utf8 COLLATE utf8_bin COMMENT '生成文章正文',           
           `CREATETIMEJZ` datetime DEFAULT CURRENT_TIMESTAMP,
           `UPDATETIMEJZ` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
            PRIMARY KEY (`id`),
-           UNIQUE KEY `un2` (`Date`) 
-        ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='资讯-早盘主力十大净买个股';
+           UNIQUE KEY `dt_type` (`Date`, `NewsType`)
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='资讯生成表';
         '''.format(self.target_table)
         self.target_client.insert(sql)
         self.target_client.end()
@@ -57,12 +86,6 @@ class MorningTop10(NewsBase):
         为了减少请求次数 比较粗暴地请求 50 个 从其中筛选出 10 个
         """
         items = self.get_rank10()
-
-        # for one in items:
-        #     print(one)
-        #
-        # print("* " * 20)
-
         processed_items = []
         count = 0
         for item in items:
@@ -110,55 +133,40 @@ class MorningTop10(NewsBase):
         return items
 
     def get_content(self, datas: list):
-        """
-        早盘主力十大净买个股
-        标题：4月15日早盘，十大主力净买个股（截取今日10点半前数据）
-        条件：取每天十点半时，主力净买额前十的个股
-
-        内容：
-        4月15日大单金额流入前十名个股如下，数据取自（4月15日 10:30）
-        山河药辅（300452）+1.54%，主力净买额1200万    (实时涨跌幅)
-        皇氏集团（002329）+1.54%，主力净买额1200万
-        以岭药业（002603）+1.54%，主力净买额1200万
-        海伦哲（300201）  +1.54%，主力净买额1200万
-        宝色股份（300402）+1.54%，主力净买额1200万
-        延江股份（300658）+1.54%，主力净买额1200万
-        龙宇燃油（603003）+1.54%，主力净买额1200万
-        泉峰汽车（603982）+1.54%，主力净买额1200万
-        """
         final = dict()
-        _today = datetime.datetime.combine(datetime.datetime.now(), datetime.time.min)
-        final["Date"] = _today
-        _month = _today.month
-        _day = _today.day
+        final["Date"] = self.day
 
+        _month = self.day.month
+        _day = self.day.day
         title = "{}月{}日早盘，十大主力净买个股（截取今日10点半前数据）".format(_month, _day)
         final['Title'] = title
+
         base_content = '{}月{}日大单金额流入前十名个股如下，数据取自（{}月{}日 10:30）\n'.format(_month, _day, _month, _day)
-        # {'secu_code': 'SZ002129', 'secu_abbr': '中环股份', 'main_buy': 688491328.0,
-        # 'rise_percent': 10.025842666625977, 'rank_num': 1}
-        # 山河药辅（300452）+1.54%，主力净买额1200万
+
         for data in datas:
             content = '{}（{}）{}%，主力净买额{}\n'.format(data.get("secu_abbr"), data.get("secu_code")[2:], data.get("rise_percent"), data.get("main_buy"))
             base_content += content
 
         final["Content"] = base_content
+
+        final['NewsType'] = 3
         return final
 
     def start(self):
+        is_trading = self.is_trading_day(self.day)
+        if not is_trading:
+            logger.warning("非交易日")
+            return
+
         self._create_table()
+
         top10info = self.dynamic_get_rank10()
 
-        # for one in top10info:
-        #     print(one)
-
         to_insert = self.get_content(top10info)
-        # print(pprint.pformat(to_insert))
 
-        self._target_init()
         ret = self._save(self.target_client, to_insert, self.target_table, self.fields)
-        if ret:
-            self.ding("早盘十大成交股资讯生成\n {}".format(pprint.pformat(to_insert)))
+
+        self.ding("早盘十大成交股资讯生成\n {}".format(pprint.pformat(to_insert)))
 
 
 def task():
@@ -168,13 +176,30 @@ def task():
 
 if __name__ == "__main__":
     # task()
-
     schedule.every().day.at("10:30").do(task)
 
     while True:
-        # print("当前调度系统中的任务列表 {}".format(schedule.jobs))
         schedule.run_pending()
         time.sleep(10)
+
+
+"""
+早盘主力十大净买个股
+标题：4月15日早盘，十大主力净买个股（截取今日10点半前数据）
+条件：取每天十点半时，主力净买额前十的个股
+
+内容：
+4月15日大单金额流入前十名个股如下，数据取自（4月15日 10:30）
+山河药辅（300452）+1.54%，主力净买额1200万    (实时涨跌幅)
+皇氏集团（002329）+1.54%，主力净买额1200万
+以岭药业（002603）+1.54%，主力净买额1200万
+海伦哲（300201）  +1.54%，主力净买额1200万
+宝色股份（300402）+1.54%，主力净买额1200万
+延江股份（300658）+1.54%，主力净买额1200万
+龙宇燃油（603003）+1.54%，主力净买额1200万
+泉峰汽车（603982）+1.54%，主力净买额1200万
+"""
+
 
 '''进入根目录下进行部署 
 docker build -f DockerfileUseApi -t registry.cn-shenzhen.aliyuncs.com/jzdev/jzdata/newsgenerator:v2 .
