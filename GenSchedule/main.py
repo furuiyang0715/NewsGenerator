fa35dccd7180389c32ccd679db38b78ce0e65b9f
furuiyang <furuiyang0715@gmail.com>
2020-07-09 14:51:44 +0800

    Add some

--- GenSchedule/main.py
+++ GenSchedule/main.py
@@ -3,17 +3,23 @@ import time
 import schedule
 
 from Funds.days3_top10 import Stocks3DaysTop10
+from Funds.limit_up_lb import LimitUpLb
 
 
 def task_1():
     # 1: 3日净流入前十个股
     Stocks3DaysTop10().start()
 
-    #
+
+def task_2():
+    # 2: 连板股今日竞价表现
+    LimitUpLb().start()
 
 
 def main():
     schedule.every().day.at("15:05").do(task_1)
+
+    schedule.every().day.at("09:25").do(task_2)
 
     while True:
         print("当前调度系统中的任务列表 {}".format(schedule.jobs))
