14b9a1700d277edf32f4e2681cf22527a4418009
furuiyang <furuiyang0715@gmail.com>
2020-07-09 10:31:00 +0800

    bug fixed

--- Funds/open_unusual.py
+++ Funds/open_unusual.py
@@ -82,6 +82,11 @@ class OpenUnusual(NewsBase):
     def get_all_block_stats(self, req_day):
         """
         获取某个请求时间点的版块数据
+        需求更新之前是
+        {'IX850003': [{'code': 'SZ300124', 'stats': 3},
+                          {'code': 'SZ002184', 'stats': 1},
+                          {'code': 'SZ300293', 'stats': 1}], ...
+        版块代码：[{领涨股,  }, {跟涨股,  }, {跟涨股, }], ...
         """
         _year, _month, _day = req_day.year, req_day.month, req_day.day
         target_time = datetime.datetime(_year, _month, _day, 9, 36, 0)
@@ -91,6 +96,8 @@ class OpenUnusual(NewsBase):
         # 拿到全部的版块列表 并且保存全部的版块数据
         block_stats_map = defaultdict(list)
         for block in res.msg_array:
+            # print("* " * 20)
+            # print(block)
             block_code = block.block_code
             stock_monitor = block.stock_monitor
             for one in stock_monitor:
@@ -121,6 +128,29 @@ class OpenUnusual(NewsBase):
                     block_rise_map[block_code] = value
         return block_rise_map
 
+    def get_first3_rise_map(self, all_block_codes):
+        """
+        查出版块涨幅前 3 的股票
+        """
+        block_rise_map = {}
+        rank = Rank.sync_get_rank_by_bk(self.client,
+                                        offset=0,
+                                        # count=len(all_block_codes),
+                                        count=3,
+                                        stock_code_array=all_block_codes
+                                        )
+        for one in rank.row:
+            block_code = one.stock_code
+            value = None
+            for i in one.data:
+                if i.type == 1:
+                    value = struct.unpack("<f", i.value)[0]
+                elif i.type == 3:
+                    print(bytes.fromhex(i.value.hex()).decode("utf-8"))
+                block_rise_map[block_code] = value
+                # print(value)
+        return block_rise_map
+
     def get_content(self, datas, block_rise_map):
         """
         {'IX850039': [{'code': 'SZ300459', 'stats': 3},
@@ -128,42 +158,53 @@ class OpenUnusual(NewsBase):
                       {'code': 'SH600070', 'stats': 1}],
                       ......
 
+        datas 指的是全部的版块 以及 该版块的领涨股、涨幅等信息
+
+        block_rise_map 指的是股票的实时涨跌幅大于 1.5% 的
+        或者在最新版本中涨幅前三的
 
         eg.
         标题：5G板块开盘活跃,涨幅高达1.5%
         内容：
         5G板块开盘活跃，盘口涨幅达1.5%，华星创业一字涨停，麦捷科技大涨9%，有方科技涨8%。
         """
+        # 将 block_rise_map 先转换为列表 再按照涨幅进行排序
+        block_rise_list = [(block, rise) for block, rise in block_rise_map.items()]
+        block_rise_list = sorted(block_rise_list, key=lambda one: one[1], reverse=True)
+
         self._theme_init()
         block_names = []
         rows_str = ''
-        for block_code, block_info in datas.items():
-            if block_code in block_rise_map:
-                sql = """select name from block where code = '{}';""".format(block_code)
-                block_name = self.theme_client.select_one(sql)
-                if block_name:
-                    block_name = block_name.get("name")
-                    block_names.append(block_name)
-                else:
-                    raise
-                block_rise = self.re_decimal_data(block_rise_map.get(block_code))
-                count = 1
-                base_content = '{}版块竞价涨幅达{}%，'.format(block_name, block_rise)
-                for one in block_info:
-                    if count > 3:
-                        break
-                    code = one.get("code")
-                    stats = one.get("stats")
-                    if count == 1:    # 首次循环出的是领涨股 之后的是跟涨股
-                        content = self.get_code_rise_info(code, stats, lead=1)
-                    else:
-                        content = self.get_code_rise_info(code, stats, lead=0)
-                    base_content += content
-                    count += 1
+        for block_code, rise in block_rise_list:
+            sql = """select name from block where code = '{}';""".format(block_code)
+            block_name = self.theme_client.select_one(sql)
+            # 版块中文名
+            if block_name:
+                block_name = block_name.get("name")
+                block_names.append(block_name)
+            else:
+                raise
+            # 版块涨幅
+            block_rise = self.re_decimal_data(rise)
+            base_content = '{}版块竞价涨幅达{}%，'.format(block_name, block_rise)
 
-                # ，后面有个空格
-                row = base_content[:-2] + "。\n"
-                rows_str += row
+            # 获取版块的 领涨股/跟涨股 情况
+            block_info = datas.get(block_code)
+            count = 1
+            for one in block_info:
+                if count > 3:
+                    break
+                code = one.get("code")
+                stats = one.get("stats")
+                if count == 1:  # 首次循环出的是领涨股 之后的是跟涨股
+                    content = self.get_code_rise_info(code, stats, lead=1)
+                else:
+                    content = self.get_code_rise_info(code, stats, lead=0)
+                base_content += content
+                count += 1
+            # ，后面有个空格
+            row = base_content[:-2] + "。\n"
+            rows_str += row
 
         if not block_names:
             logger.warning("今日无数据生成")
@@ -208,23 +249,6 @@ class OpenUnusual(NewsBase):
                 else:
                     return "{}跟涨{}%, ".format(secu_abbr, self.re_decimal_data(value))
 
-    # def _create_table(self):
-    #     self._target_init()
-    #     sql = '''
-    #     CREATE TABLE IF NOT EXISTS `{}` (
-    #       `id` int(11) NOT NULL AUTO_INCREMENT,
-    #       `Date` datetime NOT NULL COMMENT '资讯发布时间',
-    #       `Title` varchar(64) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '生成文章标题',
-    #       `Content` text CHARACTER SET utf8 COLLATE utf8_bin COMMENT '生成文章正文',
-    #       `CREATETIMEJZ` datetime DEFAULT CURRENT_TIMESTAMP,
-    #       `UPDATETIMEJZ` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    #        PRIMARY KEY (`id`),
-    #        UNIQUE KEY `un2` (`Date`)
-    #     ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='开盘异动盘口';
-    #     '''.format(self.target_table)
-    #     self.target_client.insert(sql)
-    #     self.target_client.end()
-
     def _create_table(self):
         """
         新闻类型：
@@ -262,21 +286,25 @@ class OpenUnusual(NewsBase):
         self._create_table()
 
         all_block_stats_map = self.get_all_block_stats(self.day)
-        print(pprint.pformat(all_block_stats_map))   # 非交易日无数据
+        # print(pprint.pformat(all_block_stats_map))   # 非交易日无数据
 
         if not all_block_stats_map:
             logger.warning("未获取到交易日的 block 信息")
             return
 
         all_block_codes = list(all_block_stats_map.keys())
-        print(all_block_codes)
+        # print(all_block_codes)
 
-        block_rise_map = self.get_block_rise_map(all_block_codes)
-        print(pprint.pformat(block_rise_map))     # 在非交易日是有数据的
-        rise_block_codes = list(block_rise_map.keys())
-        print(rise_block_codes)
+        # 按照修改之前的需求： 首先找出实时涨跌幅大于 1.5% 的；
+        # 然后用第一步的数据在全部的版块信息里面去截取符合条件的版块。
+        # 更新后的需求，在全部的版块中取涨幅前三的。不再设置实时涨跌幅上面的阈值。
 
-        final = self.get_content(all_block_stats_map, block_rise_map)
+        # block_rise_map = self.get_block_rise_map(all_block_codes)
+        # print(pprint.pformat(block_rise_map))     # 在非交易日是有数据的
+        first3_block_rise_map = self.get_first3_rise_map(all_block_codes)
+        print(pprint.pformat(first3_block_rise_map))
+
+        final = self.get_content(all_block_stats_map, first3_block_rise_map)
         print(pprint.pformat(final))
 
         if not final:
@@ -292,7 +320,7 @@ def task():
 
 
 if __name__ == "__main__":
-    # task()
+    task()
 
     schedule.every().day.at("09:36").do(task)
 
