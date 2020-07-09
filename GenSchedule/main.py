180f2c0d67f4f49479183cba71bea0b5ddab971c
furuiyang <furuiyang0715@gmail.com>
2020-07-09 15:08:15 +0800

    Add some

--- GenSchedule/main.py
+++ GenSchedule/main.py
@@ -5,6 +5,7 @@ import schedule
 from Funds.days3_top10 import Stocks3DaysTop10
 from Funds.limit_up_lb import LimitUpLb
 from Funds.morning_top10 import MorningTop10
+from Funds.open_unusual import OpenUnusual
 
 
 def task_1():
@@ -22,12 +23,19 @@ def task_3():
     MorningTop10().start()
 
 
+def task_4():
+    # 4:  开盘异动盘口
+    OpenUnusual().start()
+
+
 def main():
     schedule.every().day.at("15:05").do(task_1)
 
     schedule.every().day.at("09:25").do(task_2)
 
     schedule.every().day.at("10:30").do(task_3)
+
+    schedule.every().day.at("09:36").do(task_4)
 
     while True:
         print("当前调度系统中的任务列表 {}".format(schedule.jobs))
